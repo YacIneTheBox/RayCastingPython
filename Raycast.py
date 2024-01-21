8a45e1ea0ce65528b0d8ccf2ab22b3aca3754aad
YacIneTheBox <111428692+YacIneTheBox@users.noreply.github.com>
2024-01-21 08:51:06 +0100

    Add files via upload

--- Raycast.py
+++ Raycast.py
@@ -2,64 +2,65 @@ import math
 import pygame
 import sys
 from shapely.geometry import LineString
-import random
 
+# Initialisation de Pygame
 pygame.init()
 
+# Dimensions de la fenêtre
 w, h = 800, 600
 
+# Couleurs
 color = (255, 255, 255)
+color1 = (194, 194, 6)
 
-startp = (50, 50)
-
-endp = (200, 200)
-
+# Rectangle mobile contrôlé par la souris
 square_rect = pygame.Rect(420, 100, 10, 5)
 
+# Création de la fenêtre
 screen = pygame.display.set_mode((w, h))
-
 pygame.display.set_caption("RayCast")
 
-wall1 = (startp[0], startp[1], endp[0], endp[1])
-lwall1 = LineString([(startp[0], startp[1]), (endp[0], endp[1])])
+# Définition des murs (segments de lignes)
+wall1 = (50, 50, 200, 200)
+lwall1 = LineString([(50, 50), (200, 200)])
+
 wall2 = (900, 200, 400, 150)
 lwall2 = LineString([(wall2[0], wall2[1]), (wall2[2], wall2[3])])
+
 wall3 = (600, 500, 300, 400)
 lwall3 = LineString([(wall3[0], wall3[1]), (wall3[2], wall3[3])])
+
 wall4 = (300, 600, 100, 300)
 lwall4 = LineString([(wall4[0], wall4[1]), (wall4[2], wall4[3])])
+
+# Longueur des rayons
 line_length = 1000
 
+# Listes contenant tous les murs (segments de lignes) et leurs représentations Shapely
 lwalls = [lwall1, lwall2, lwall3, lwall4]
 walls = [wall1, wall2, wall3, wall4]
-# boucle
 
+# Boucle principale
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # the game
-    #color1 = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    color1 = (194, 194, 6)
+    # Mouvement du rectangle avec la souris
     mouse_x, mouse_y = pygame.mouse.get_pos()
     square_rect.x = mouse_x - square_rect.width / 2
     square_rect.y = mouse_y - square_rect.height / 2
 
-    # dessin
-
+    # Dessin de la scène
     screen.fill((54, 59, 58))
-
-    pygame.draw.line(screen, color, startp, endp, 2)
+    pygame.draw.line(screen, color, (wall1[0], wall1[1]), (wall1[2], wall1[3]), 2)
     pygame.draw.line(screen, color, (wall2[0], wall2[1]), (wall2[2], wall2[3]), 2)
     pygame.draw.line(screen, color, (wall3[0], wall3[1]), (wall3[2], wall3[3]), 2)
     pygame.draw.line(screen, color, (wall4[0], wall4[1]), (wall4[2], wall4[3]), 2)
-
     pygame.draw.rect(screen, color, square_rect)
 
-    # creation of the rays
-    i = 0
+    # Création des rayons
     for i in range(360):
         angle_degree = i
         angle_radians = math.radians(angle_degree)
@@ -68,36 +69,33 @@ while True:
             square_rect.centery + int(line_length * math.sin(angle_radians))
         )
         lray = LineString([(square_rect.x, square_rect.y), end_point])
-        z = 0
+
+        # Initialisation des variables
         intersection_point = end_point
-        mini = math.sqrt((mouse_x - wall2[0]) ** 2 + (mouse_y - wall2[1]) ** 2)
-        cmpt = 0
-        # boucle de checking de wall chaque ray est test avec tous les wall
         distance_wall = []
 
-        # detecter quel wall est le plus proche
+        # Détection du mur le plus proche
         for j in range(len(lwalls)):
             midpointx = ((walls[j][0] + walls[j][2])/2)
-            midpointy = ((walls[j][1] + walls[j][2])/2)
+            midpointy = ((walls[j][1] + walls[j][3])/2)
             distance_wall.append(math.sqrt((mouse_x - midpointx) ** 2 + (mouse_y - midpointy) ** 2))
-
 
         valeur_minimale = min(distance_wall)
         closest = distance_wall.index(valeur_minimale)
         lwalls[0], lwalls[closest] = lwalls[closest], lwalls[0]
         walls[0], walls[closest] = walls[closest], walls[0]
 
+        # Détection des intersections avec les murs
         for z in range(len(lwalls)):
-
-            j = 0
             if lray.intersects(lwalls[z]):
                 intersection = lray.intersection(lwalls[z])
-                cmpt += 1
                 if intersection.geom_type == 'Point':
                     intersection_point = (int(intersection.x), int(intersection.y))
                     break
 
+        # Dessin du rayon
         pygame.draw.line(screen, color1, square_rect.center, (intersection_point[0], intersection_point[1]), 1)
 
+    # Mise à jour de l'écran
     pygame.display.flip()
     pygame.time.Clock().tick(60)
